f8d884d3693175e8034a80fa9931897b69e415d2
Jim Eli <31859133+JimEli@users.noreply.github.com>
2020-01-12 09:30:59 -0700

    Update serial_port.h

--- serial_port.h
+++ serial_port.h
@@ -7,54 +7,29 @@
 extern "C" {
 #endif
 
-#define CP_BAUD_RATE_9600 CBR_9600
-#define CP_BAUD_RATE_1155 1155
-#define CP_BAUD_RATE_4800 CBR_4800
-#define CP_BAUD_RATE_19200 CBR_19200
-
-#define CP_DATA_BITS_5 5
-#define CP_DATA_BITS_6 6
-#define CP_DATA_BITS_7 7
-#define CP_DATA_BITS_8 8
-
-#define CP_STOP_BITS_ONE ONESTOPBIT
-#define CP_STOP_BITS_TWO TWOSTOPBITS
-#define CP_STOP_BITS_ONE_AND_HALF ONE5STOPBITS
-
-#define CP_PARITY_NOPARITY NOPARITY
-#define CP_PARITY_ODD ODDPARITY
-#define CP_PARITY_EVEN EVENPARITY
-#define CP_PARITY_MARK MARKPARITY
-#define CP_PARITY_SPACE SPACEPARITY
-
 typedef HANDLE PORT;
 
 // Open comm serial port specified by idx.
 // Returns a handle to the opend port, on error returns NULL.
-PORT OpenPort(int idx);
+PORT OpenPort(unsigned idx);
 
 // Close the handle of the opend port.
 void ClosePort(PORT com_port);
 
 // Set the baud rate of the specified com_port.
 // Returns TRUE if operation successful otherwise returns FALSE.
-// NOTE: You can use the values of CP_BAUD_RATE to feed the rate parameter.
 int SetPortBaudRate(PORT com_port, int rate);
 
 // Set the data bits of the specified com_port.
 // Returns TRUE if operation successful otherwise returns FALSE.
-// NOTE: You can use the values of CP_DATA_BITS to feed the databits parameter.
-// NOTE: Becareful what you send if you set this other than 8 bits.
 int SetPortDataBits(PORT com_port, int databits);
 
 // Set the stop bits of the specified com_port.
 // Returns TRUE if operation successful otherwise returns FALSE.
-// NOTE: Use the values of CP_STOP_BITS to feed the stopbits parameter.
 int SetPortStopBits(PORT com_port, int stopbits);
 
 // Set the parity type of the specified com_port.
 // Returns TRUE if operation successful otherwise returns FALSE.
-// NOTE: Use the values of CP_PARITY to feed the parity parameter.
 int SetPortParity(PORT com_port, int parity);
 
 // Get the baud rate of the specified com_port.
